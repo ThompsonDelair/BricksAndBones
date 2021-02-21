6071b193b538ba03d89204457443897ab7c07a90
Thompson <messagethompson@gmail.com>
2021-02-20 20:07:37 -0800

    added OpenGL

---
+++ BricksAndBones/Vertex.swift
@@ -0,0 +1,18 @@
+//
+//  Vertex.swift
+//  BricksAndBones
+//
+//  Created by Thompson DeLair-Dobrovolny on 2021-02-20.
+//
+
+import GLKit
+
+struct Vertex{
+    var x: GLfloat
+    var y: GLfloat
+    var z: GLfloat
+    var r: GLfloat
+    var g: GLfloat
+    var b: GLfloat
+    var a: GLfloat
+}
