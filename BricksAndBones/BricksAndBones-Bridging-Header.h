4fe0581bbb2e1400e8185300f3d7e3f64cb98581
Adam King <adamkdevelop@gmail.com>
2021-03-03 15:31:10 -0800

    cleaned up code and removed old classes

--- BricksAndBones/BricksAndBones-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  CubeObject-Bridge.h
-//  BricksAndBones
-//
-//  Created by socas on 2021-03-02.
-//
-
-#import "CubeObject.h"
--- BricksAndBones/ViewController.swift
+++ BricksAndBones/ViewController.swift
@@ -33,16 +33,6 @@ class ViewController: GLKViewController {
     
     private var scoreLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
     
-    //private var Cubes: [CubeObject] = [];
-    
-    /*
-    var Vertices = [
-        Vertex(x: 1, y: -1, z: 0, r: 1, g: 0, b: 0, a: 1),
-        Vertex(x: 1, y: 1, z: 0, r: 0, g: 1, b: 0, a: 1),
-        Vertex(x: -1, y: 1, z: 0, r: 0, g: 0, b: 1, a: 1),
-        Vertex(x: -1, y: -1, z: 0, r: 0, g: 0, b: 0, a: 1)
-    ]*/
-    
     var CubeVerts : [[Float]] =
     [
         [-0.5, -0.5, -0.5],
@@ -98,17 +88,8 @@ class ViewController: GLKViewController {
     
     var gameGrid: Grid = Grid(unitSize: 2);
     
-    /*
-    var Indices: [GLubyte] = [
-        0,1,2,
-        2,3,0
-    ]
-    */
     override func viewDidLoad() {
         super.viewDidLoad();
-        //var cubeObj = type(of: CubeObject()).init();
-        //print("cube object \(cubeObj.position.x)")
-        //Cubes.append(cubeObj);
         
         // to do anything with OpenGL, you need to create an EAGLContext
         context = EAGLContext(api: .openGLES3)
@@ -147,30 +128,6 @@ class ViewController: GLKViewController {
         scoreLabel.textAlignment = .center
         self.view.addSubview(scoreLabel)
         
-        //loadCubes()
-        /*
-        //loadModels()
-        //setupGL()
-        //setupGL_Arg(name: "cube1")
-        //setupGL_Arg(name: "cube2")
-        //setupGL_Arg(name: "cube2")
-        print("did load")
-       
-        drawButton()
-        
-//        var grid = Grid(unitSize:4)
-//        var position:(Float, Float) = (2.5, 11)
-//        var position2:(Float, Float) = (12.5, 14.5)
-//
-//        print("snapping numbers x: \(position.0) y: \(position.1)")
-//        var snappedPosition = grid.snapToGrid(x: position.0, y: position.1)
-//        print("snapped position x: \(snappedPosition.0) y:  \(snappedPosition.1)")
-//        print("snapping numbers x: \(position2.0) y: \(position2.1)")
-//        var snappedPosition2 = grid.snapToGrid(x:position2.0, y:position2.1)
-//        print("snapped position2 x: \(snappedPosition2.0) y:  \(snappedPosition2.1)")
-    
-
- */
     }
     
     @objc func handleTap(_ sender: UITapGestureRecognizer){
@@ -313,62 +270,6 @@ class ViewController: GLKViewController {
         TranslationDict["cube\(CubeCounter)"] = Vertex(x: Float(CubeCounter * 2), y: Float(CubeCounter * 2), z: Float(CubeCounter * 2), r: 0, g: 0, b: 0, a: 0)
         CubeCounter += 1;
     }
-    /*
-    func loadCubes(){
-        var CubeVerts : [[Float]] =
-        [
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5,  0.5],
-            [0.5, -0.5,  0.5],
-            [0.5, -0.5, -0.5],
-            [-0.5,  0.5, -0.5],
-            [-0.5,  0.5,  0.5],
-            [0.5,  0.5,  0.5],
-            [0.5,  0.5, -0.5],
-            [-0.5, -0.5, -0.5],
-            [-0.5,  0.5, -0.5],
-            [0.5,  0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [-0.5, -0.5, 0.5],
-            [-0.5,  0.5, 0.5],
-            [0.5,  0.5, 0.5],
-            [0.5, -0.5, 0.5],
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5,  0.5],
-            [-0.5,  0.5,  0.5],
-            [-0.5,  0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5,  0.5],
-            [0.5,  0.5,  0.5],
-            [0.5,  0.5, -0.5],
-        ];
-        
-        for vert in CubeVerts{
-            VertDict["cube1", default: []].append(Vertex(x: vert[0]-2, y:vert[1]-2, z:vert[2]-2, r: 1, g: 0, b: 1, a: 1))
-            VertDict["cube2", default: []].append(Vertex(x: vert[0], y:vert[1], z:vert[2], r: 1, g: 0, b: 0, a: 1))
-        }
-        
-        var CubeIndices : [GLubyte] =
-        [
-            0, 2, 1,
-            0, 3, 2,
-            4, 5, 6,
-            4, 6, 7,
-            8, 9, 10,
-            8, 10, 11,
-            12, 15, 14,
-            12, 14, 13,
-            16, 17, 18,
-            16, 18, 19,
-            20, 23, 22,
-            20, 22, 21
-        ];
-        
-        for indicies in CubeIndices{
-            IndexDict["cube1", default:[]].append(indicies)
-            IndexDict["cube2", default:[]].append(indicies)
-        }
-    }*/
     
     func loadModels(){
         if let path = Bundle.main.path(forResource: "rook", ofType: "obj"){
@@ -415,90 +316,7 @@ class ViewController: GLKViewController {
                            nil)
             i += 1;
         }
-        /*
-        setupGL_Arg(name: "cube1")
-        // binds and compiles shaders for us
-        effect.prepareToDraw()
-        glBindVertexArrayOES(vao)
-        glDrawElements(GLenum(GL_TRIANGLES),        // tells OpenGL what you want to draw (we wanna draw triangles)
-                       GLsizei(IndexDict["cube1", default:[]].count),          // tells OpenGL how many vertices you want to draw
-                       GLenum(GL_UNSIGNED_BYTE),          // specifies the type of values contained in each index
-                       nil)                           // specifies offset within a buffer
-        
-        setupGL_Arg(name: "cube2")
-        effect.prepareToDraw()
-        glBindVertexArrayOES(vao)
-        glDrawElements(GLenum(GL_TRIANGLES),        // tells OpenGL what you want to draw (we wanna draw triangles)
-                       GLsizei(IndexDict["cube1", default:[]].count),          // tells OpenGL how many vertices you want to draw
-                       GLenum(GL_UNSIGNED_BYTE),          // specifies the type of values contained in each index
-                       nil)
- */
     }
-    private func setupGL(){
-        // to do anything with OpenGL, you need to create an EAGLContext
-        context = EAGLContext(api: .openGLES3)
-        // specify that the rendering context is the one to use in the current thread
-        EAGLContext.setCurrent(context);
-        
-        if let view = self.view as? GLKView, let context = context{
-            //set the GLKView context
-            view.context = context
-            // set the current class as the GLKViewController's delegate
-            delegate = self
-        }
-        
-        // specify how to read colors from vertex data structures
-        let vertexAttribColor  = GLuint(GLKVertexAttrib.color.rawValue)
-        // specify how to read position from vertex data struture
-        let vertexAttribPosition = GLuint(GLKVertexAttrib.position.rawValue)
-        // specify the size, in bytes, of vertex item
-        let vertexSize = MemoryLayout<Vertex>.stride
-        // memory offset of variables corresponding to vertex color
-        let colorOffset = MemoryLayout<GLfloat>.stride * 3
-        // convert offset into the required type
-        let colorOffsetPointer = UnsafeRawPointer(bitPattern: colorOffset)
-        
-        // ask OpenGL to generate a new VAO
-        glGenVertexArraysOES(1, &vao)
-        // tell OpenGL to bind the VAO and that upcoming calls to configure vertex attribute pointers should be stored in this VAO
-        glBindVertexArrayOES(vao)
-        
-        
-        glBufferData(GLenum(GL_ARRAY_BUFFER),   // indicates to what buffer we are passing data
-                     VertDict["cube1", default: []].size(),               // specify the size, in bytes, of the data
-                     VertDict["cube1"],                      // the data we are going to use
-                     GLenum(GL_STATIC_DRAW))        // tell OpenGL how we want the GPU to manage the data
-                    // !! in this tutorial the data we are passing to the graphics card will rarely change, if at all
-                    // !!   this allows OpenGL to optimize for this scenario
-        
-        glEnableVertexAttribArray(vertexAttribPosition)
-        glVertexAttribPointer(vertexAttribPosition,             // specifies the attribute name to set
-                              3,                                    // specifies how many values are present for each vertex (3 position floats, 4 color floats)
-                              GLenum(GL_FLOAT),                     // specifies the type of each value
-                              GLboolean(UInt8(GL_FALSE)),             // specify if you want the data to be normalized
-                              GLsizei(vertexSize),                      // the size of the data structure containing the per-vertext data when its in an array
-                              nil)                   // offset of the position data
-        
-        glEnableVertexAttribArray(vertexAttribColor)
-        glVertexAttribPointer(vertexAttribColor,
-                              4,
-                              GLenum(GL_FLOAT),
-                              GLboolean(UInt8(GL_FALSE)),
-                              GLsizei(vertexSize),
-                              colorOffsetPointer)
-        
-        glGenBuffers(1, &ebo)
-        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), ebo)
-        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER),
-                     IndexDict["cube1", default: []].size(),
-                     IndexDict["cube1", default: []],
-                     GLenum(GL_STATIC_DRAW))
-        
-        glBindVertexArrayOES(0)
-        glBindBuffer(GLenum(GL_ARRAY_BUFFER), 0)
-        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), 0)
-    }
-    
     private func TranslateVerts(inputVert: [Vertex], inputTranslation: Vertex) -> [Vertex]{
         var outputVert :[Vertex] = inputVert;
         
@@ -566,73 +384,6 @@ class ViewController: GLKViewController {
         glBindBuffer(GLenum(GL_ARRAY_BUFFER), 0)
         glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), 0)
     }
-    /*
-    private func setupGL(){
-        // to do anything with OpenGL, you need to create an EAGLContext
-        context = EAGLContext(api: .openGLES3)
-        // specify that the rendering context is the one to use in the current thread
-        EAGLContext.setCurrent(context);
-        
-        if let view = self.view as? GLKView, let context = context{
-            //set the GLKView context
-            view.context = context
-            // set the current class as the GLKViewController's delegate
-            delegate = self
-        }
-        
-        // specify how to read colors from vertex data structures
-        let vertexAttribColor  = GLuint(GLKVertexAttrib.color.rawValue)
-        // specify how to read position from vertex data struture
-        let vertexAttribPosition = GLuint(GLKVertexAttrib.position.rawValue)
-        // specify the size, in bytes, of vertex item
-        let vertexSize = MemoryLayout<Vertex>.stride
-        // memory offset of variables corresponding to vertex color
-        let colorOffset = MemoryLayout<GLfloat>.stride * 3
-        // convert offset into the required type
-        let colorOffsetPointer = UnsafeRawPointer(bitPattern: colorOffset)
-        
-        // ask OpenGL to generate a new VAO
-        glGenVertexArraysOES(1, &vao)
-        // tell OpenGL to bind the VAO and that upcoming calls to configure vertex attribute pointers should be stored in this VAO
-        glBindVertexArrayOES(vao)
-        
-        glGenBuffers(1, &vbo)
-        glBindBuffer(GLenum(GL_ARRAY_BUFFER), vbo)
-        glBufferData(GLenum(GL_ARRAY_BUFFER),   // indicates to what buffer we are passing data
-                     Vertices.size(),               // specify the size, in bytes, of the data
-                     Vertices,                      // the data we are going to use
-                     GLenum(GL_STATIC_DRAW))        // tell OpenGL how we want the GPU to manage the data
-                    // !! in this tutorial the data we are passing to the graphics card will rarely change, if at all
-                    // !!   this allows OpenGL to optimize for this scenario
-        
-        glEnableVertexAttribArray(vertexAttribPosition)
-        glVertexAttribPointer(vertexAttribPosition,             // specifies the attribute name to set
-                              3,                                    // specifies how many values are present for each vertex (3 position floats, 4 color floats)
-                              GLenum(GL_FLOAT),                     // specifies the type of each value
-                              GLboolean(UInt8(GL_FALSE)),             // specify if you want the data to be normalized
-                              GLsizei(vertexSize),                      // the size of the data structure containing the per-vertext data when its in an array
-                              nil)                   // offset of the position data
-        
-        glEnableVertexAttribArray(vertexAttribColor)
-        glVertexAttribPointer(vertexAttribColor,
-                              4,
-                              GLenum(GL_FLOAT),
-                              GLboolean(UInt8(GL_FALSE)),
-                              GLsizei(vertexSize),
-                              colorOffsetPointer)
-        
-        glGenBuffers(1, &ebo)
-        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), ebo)
-        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER),
-                     Indices.size(),
-                     Indices,
-                     GLenum(GL_STATIC_DRAW))
-        
-        glBindVertexArrayOES(0)
-        glBindBuffer(GLenum(GL_ARRAY_BUFFER), 0)
-        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), 0)
-    }
- */
     
     private func tearDownGL(){
         CVEAGLContext.setCurrent(context)
@@ -704,9 +455,6 @@ extension ViewController: GLKViewControllerDelegate{
         
         // translation
         var modelViewMatrix = GLKMatrix4MakeTranslation(0.0, 0.0, -20.0)
-        // rotation
-        //rotation += 90 * Float(timeSinceLastUpdate)
-        //modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, GLKMathDegreesToRadians(rotation),0,0,1)
         // set the model view matrix on the effect's transform property
         effect.transform.modelviewMatrix = modelViewMatrix
     }
